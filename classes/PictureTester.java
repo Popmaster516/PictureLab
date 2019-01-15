47df5dee321419737c9c94193b71142c582cef8f
BT_1N3_32 <vbondmoore6034@bths.edu>
2019-01-15 11:36:54 -0500

    boom

--- classes/PictureTester.java
+++ classes/PictureTester.java
@@ -123,7 +123,7 @@ public class PictureTester
     // uncomment a call here to run a test
     // and comment out the ones you don't want
     // to run
-    //testZeroBlue();
+    testZeroBlue();
     //testKeepOnlyBlue();
     //testKeepOnlyRed();
     //testKeepOnlyGreen();
